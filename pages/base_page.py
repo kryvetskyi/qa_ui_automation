8a8429eac08faf941ed46a9ae12607548c5cf867
Anatolii Kryvetskyi <Anatol@MacBook-Anatol.local>
2022-06-29 21:47:43 +0300

    Project setup. Created venv using poetry. Added libraries for the framework.

---
+++ pages/base_page.py
@@ -0,0 +1,35 @@
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium import webdriver
+
+
+class BasePage:
+
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def is_element_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+    def are_elements_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def is_element_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    def are_elements_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    def is_element_invisible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
+
+    def is_element_clickable(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+
+    def move_to_element(self, element):
+        self.driver.execute_script("argument[0].scrollIntoView;", element)
