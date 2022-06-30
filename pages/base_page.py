c0eee1028c4155412f89f33837e0b15c676930dd
Anatolii Kryvetskyi <Anatol@MacBook-Anatol.local>
2022-06-30 19:29:45 +0300

    Remove unused imports. Added new methods to basepage.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,5 @@
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium import webdriver
 
 
 class BasePage:
@@ -31,5 +29,8 @@ class BasePage:
     def is_element_clickable(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
 
-    def move_to_element(self, element):
-        self.driver.execute_script("argument[0].scrollIntoView;", element)
+    def scroll_down(self):
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+    def go_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
