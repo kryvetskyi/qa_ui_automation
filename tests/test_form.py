5b5767f850d7dcc6bc35224a8fe384e2904866db
Anatolii <43533017+kryvetskyi@users.noreply.github.com>
2023-02-13 16:43:10 +0200

    Update test_form.py

--- tests/test_form.py
+++ tests/test_form.py
@@ -3,7 +3,6 @@ from pages.form_page import FormPage
 
 class TestForm:
     class TestFormPage:
-
         def test_form(self, driver, output_file):
             form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
             form_page.open()
