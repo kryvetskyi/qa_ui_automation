aa3e6e97cb3249f6e1b8ccd205155c45fee43767
kryvetskyi <kryvetskyi@gmail.com>
2023-07-28 17:18:21 +0300

    Added allure reports to jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,17 +5,21 @@ pipeline {
         stage('Check out') {
             steps {
                 checkout([$class: 'GitSCM', 
-                branches: [[name: '*/master']], 
+                branches: [[name: '*/jenkins_job']],
                 userRemoteConfigs: [[url: 'https://github.com/kryvetskyi/qa_ui_automation.git']]])
             }
         }
         
         stage('Build and Test') {
             steps {
+                // Remove previous test results
+                sh "rm -r reports/allure-results/*"
+
                 // Build the custom Docker image using the Dockerfile in the project directory
                 script {
                     docker.build("my_custom_image:${env.BUILD_NUMBER}", "-f Dockerfile .")
                 }
+
                 // Run the build and test steps inside the custom Docker container
                 script {
                     docker.image("my_custom_image:${env.BUILD_NUMBER}").inside {
@@ -31,8 +35,12 @@ pipeline {
         always {
             // Clean up after the build
             script {
-                sh 'echo "done"'
+                sh "docker rmi my_custom_image:${env.BUILD_NUMBER}"
             }
+            allure([
+            reportBuildPolicy: "ALWAYS",
+            results: [[path: "reports/allure-results"]]
+            ])
         }
     }
 }
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,71 +1,18 @@
-from datetime import datetime
 import random
-from pathlib import Path
-
-import allure
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-
-
-# def pytest_addoption(parser):
-    #parser.addoption("--driver", action="store", default="chrome", help="Enter browser")
-    #parser.addoption("--headless", action="store", default="headless", help="Run tests in headless mode")
 
 
 @pytest.fixture(scope="function")
-def driver(request):
+def driver():
     options = Options()
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     driver = webdriver.Remote(command_executor="http://192.168.0.164:4444/wd/hub", options=options)
     yield driver
     driver.quit()
-
-#@pytest.fixture(scope='function')
-#def driver(request):
-    # Retrieve the chosen browser from command-line options
- #   browser = request.config.getoption("--driver")
-
-    # Check if only 'chrome' is supported
-  #  if browser != 'chrome':
-   #     pytest.fail('only chrome is supported at the moment')
-
-    # Retrieve the headless option from command-line options
-    #headless = request.config.getoption("--headless")
-
-    # Configure Chrome options
-   # options = Options()
-    # options.add_argument("--start-maximized")
-
-    # Add headless mode option if requested
-    #if headless:
-      #  options.add_argument("--headless")
-
-    # Create the Chrome WebDriver instance
-    # driver = weibdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    # driver = webdriver.Remote(command_executor="http://3.67.207.173:4444/wd/hub", options=options)
-    # yield driver
-
-    # After the test execution, if the test failed, capture and attach a screenshot
-    #if request.node.rep_call.failed:
-     #   screenshot_filename, screenshot_path = create_screenshot_path()
-        #allure.attach(driver.get_screenshot_as_file(screenshot_path),
-         #             name=screenshot_filename,
-          #            attachment_type=allure.attachment_type.PNG)
-
-
-def create_screenshot_path():
-    # Generate a unique filename and path for the screenshot
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    screenshot_filename = f"screenshot_{timestamp}.png"
-    screenshot_dir = Path(__file__).resolve().parent.parent / 'reports' / 'screenshots'
-    screenshot_dir.mkdir(parents=True, exist_ok=True)
-    screenshot_path = screenshot_dir / screenshot_filename
-    return screenshot_filename, screenshot_path
 
 
 @pytest.fixture(scope="function")
@@ -83,16 +30,3 @@ def output_file(tmp_path):
 
         # Yield the full path and file name to the test
         yield tmp_file_full_path, file_name
-
-
-#@pytest.hookimpl(hookwrapper=True, tryfirst=True)
-#def pytest_runtest_makereport(item):
-    # Yield the test outcome to the hook
- #   outcome = yield
-  #  rep = outcome.get_result()
-
-    # Set an attribute on the test item with the test outcome information
-    # setattr(item, "rep_" + rep.when, rep)
-
-    # Return the test report outcome
-    #return rep
